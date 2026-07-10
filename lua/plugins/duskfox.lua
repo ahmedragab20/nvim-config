d87eb403a1d8ee2febba92091919fb962d98ecbe
ahmedragab <ahmedragab20901@gmail.com>
2026-07-10 16:55:14 +0300

    feat: switch to duskfox theme and route noice cmdline to bottom bar

--- lua/plugins/duskfox.lua
+++ lua/plugins/duskfox.lua
@@ -3,20 +3,24 @@ return {
     "EdenEast/nightfox.nvim",
     lazy = false,
     priority = 1000,
+    build = ":NightfoxCompile",
     opts = {
       options = {
-        theme = "nordfox",
+        compile_path = vim.fn.stdpath("cache") .. "/nightfox",
+        transparent = false,
+        terminal_colors = true,
+        dim_inactive = false,
       },
     },
     config = function(_, opts)
       require("nightfox").setup(opts)
-      vim.cmd.colorscheme("nordfox")
+      vim.cmd.colorscheme("duskfox")
     end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "nordfox",
+      colorscheme = "duskfox",
     },
   },
 }
