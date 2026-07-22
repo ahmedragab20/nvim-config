46b47c3ac86f7cf627529252bf0c3d476851617d
ahmedragab <ahmedragab20901@gmail.com>
2026-07-22 04:12:00 +0300

    feat: add .mdc filetype detection for Nuxt Content

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -2,6 +2,9 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
+-- .mdc (Nuxt Content Markdown Components)
+vim.filetype.add({ extension = { mdc = "markdown" } })
+
 vim.g.snacks_animate = false
 vim.g.lazyvim_php_lsp = "intelephense"
 
